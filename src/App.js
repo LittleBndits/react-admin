73587ccf7bb03c66d7bcafc294c7a861bd4fe393
chenqiulong0520@163.com <199105.10>
2020-07-14 18:41:34 +0800

    2020-07-14 18:41:29

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import "./App.scss";
+// 引用组件
+import Home from "./views/Home";
+import About from "./views/About";
+import News from "./views/News";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div class="test">
+        <h2>dfaf</h2>
+        <BrowserRouter>
+          <Switch>
+            <Route exact component={Home} path="/" />
+            <Route component={About} path="/About" />
+            <Route component={News} path="/News" />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/views/News.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+
+class News extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {}
+
+    }
+    render() {
+        return (
+            <div>
+                News 新闻
+            </div>
+        )
+    }
+}
+
+export default News
