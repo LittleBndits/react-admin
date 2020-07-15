7f5418c85fec43060d5d9b2cac1567748f10f518
chenqiulong0520@163.com <199105.10>
2020-07-15 10:00:36 +0800

    2020-07-15 10:00:32

---
+++ src/views/Login/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+// css
+import './index.scss'
+
+// 自定义组件
+import LoginForm from './loginForm'
+import RegisterForm from './registerForm'
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            formType: 'login'
+        }
+    }
+
+    onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+
+    switchForm = (val) => {
+        this.setState({
+            formType: val
+        })
+    }
+    render() {
+        return (
+            <div className="form-wrap">
+                <div>
+                    {this.state.formType === 'login' ? <LoginForm switchForm={this.switchForm}></LoginForm> : <RegisterForm switchForm={this.switchForm}></RegisterForm>}
+                </div>
+            </div>
+        )
+    }
+
+}
+export default Login;
---
+++ src/views/Login/loginForm.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react'
+// 組件
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+    toggleForm = () => {
+        this.props.switchForm('register')
+    }
+    render() {
+        return (
+            <Fragment >
+                <div className="form-header">
+                    <div className="title">登录</div>
+                    <span onClick={this.toggleForm}>账号注册</span>
+                </div>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={() => this.onFinish}
+                >
+                    <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+                        <Input prefix={<LockOutlined className="site-form-item-icon" />} placeholder="password" />
+                    </Form.Item>
+                    <Form.Item name="code" rules={[{ required: true, message: 'Please input your code!' }]}>
+                        <Row gutter={13}>
+                            <Col span={15}>
+                                <Input prefix={<LockOutlined className="site-form-item-icon" />} placeholder="code" />
+                            </Col>
+                            <Col span={9}>
+                                <Button type="danger" block>获取验证码</Button>
+                            </Col>
+                        </Row>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button" block>登录</Button>
+                    </Form.Item>
+                </Form>
+            </Fragment>
+        )
+    }
+
+}
+export default LoginForm;
